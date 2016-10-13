b41ecd176a6d5044ae1ac506fc581dffe55ad3e1
Thoriq Firdaus <tfirdaus@outlook.com>
2016-10-13 11:23:32 +0700

    improve the inline documentations

--- widgets/class-widgets.php
+++ widgets/class-widgets.php
@@ -1,55 +1,98 @@
 <?php
+/**
+ * Widgets Registration.
+ *
+ * @package     WPSocialManager
+ * @subpackage  Widgets
+ * @author      Thoriq Firdaus <tfirdau@outlook.com>
+ */
 
 namespace XCo\WPSocialManager;
 
+/**
+ * Widget Class to register custom widgets of the plugin.
+ *
+ * @since 1.0.0
+ */
 final class Widgets {
 
-	/**
-	 * [$plugin_dir description]
-	 * @var [type]
-	 */
-	protected $plugin_dir;
+    /**
+     * Common arguments passed in a Class or a function.
+     *
+     * @since   1.0.0
+     * @access  protected
+     * @var     array
+     */
+    protected $args;
 
 	/**
-	 * [__construct description]
-	 */
+     * The plugin path directory.
+     *
+     * @since   1.0.0
+     * @access  protected
+     * @var     string
+     */
+	protected $path_dir;
+
+	/**
+     * Constructor function.
+     *
+     * Load the dependencies, and run the hooks to register the custom widgets.
+     *
+     * @since 1.0.0
+     *
+     * @param array $args {
+     *     An array of common arguments of the plugin.
+     *
+     *     @type string $plugin_name    The unique identifier of this plugin.
+     *     @type string $plugin_opts    The unique identifier or prefix for database names.
+     *     @type string $version        The plugin version number.
+     * }
+     */
     public function __construct( array $args ) {
 
-        /**
-         * [$this->args description]
-         * @var [type]
-         */
         $this->args = $args;
 
-        /**
-         * [$this->plugin_dir description]
-         * @var [type]
-         */
-    	$this->plugin_dir = trailingslashit( plugin_dir_path( __FILE__ ) );
+    	$this->path_dir = plugin_dir_path( __FILE__ );
 
     	$this->requires();
         $this->hooks();
     }
 
     /**
-     * [requires description]
-     * @return [type] [description]
+     * Load the required dependencies for the widgets.
+     *
+     * @since  1.0.0
+     * @access protected
+     *
+     * @return void
      */
     protected function requires() {
     	require_once( $this->plugin_dir . 'partials/class-social-profiles.php' );
     }
 
     /**
-     * [hooks description]
-     * @return [type] [description]
+     * Run Filters and Actions required.
+     *
+     * @since  1.0.0
+     * @access protected
+     *
+     * @return void
      */
     protected function hooks() {
         add_action( 'widgets_init', array( $this, 'setups' ) );
     }
 
     /**
-     * [setups description]
-     * @return [type] [description]
+     * Run the widget setups.
+     *
+     * The setups may involve running some Classes, Functions, or WordPress Hooks
+     * that are required to render the widget properly.
+     *
+     * @since  1.0.0
+     * @access protected
+     *
+     * @return void
      */
     public function setups() {
         register_widget( new WidgetSocialProfiles( $this->args ) );
