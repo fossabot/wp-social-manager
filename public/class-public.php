543e6046d3e7921cea3a8ad1e22952505506d0dd
Thoriq Firdaus <tfirdaus@outlook.com>
2016-10-16 03:26:20 +0700

    add is_stylesheet condition

--- public/class-public.php
+++ public/class-public.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The public-facing functionality of the plugin.
+ * Public: ViewPublic class
  *
  * @package 	WPSocialManager
  * @subpackage 	Public
@@ -13,54 +13,63 @@ namespace XCo\WPSocialManager;
  * The public-facing functionality of the plugin.
  *
  * Defines the plugin name, version, enqueue the stylesheet and JavaScript,
- * and register custom API Routes with the built-in WP-API infrastructure.
+ * and register custom API Routes using the built-in WP-API infrastructure.
  *
  * @since 1.0.0
  */
-final class ViewPublic {
+final class ViewPublic extends OutputUtilities {
 
 	/**
 	 * Common arguments passed in a Class or a function.
 	 *
-	 * @since  	1.0.0
-	 * @access 	protected
-	 * @var 	array
+	 * @since 1.0.0
+	 * @access protected
+	 * @var array
 	 */
 	protected $args;
 
 	/**
 	 * The ID of this plugin.
 	 *
-	 * @since  	1.0.0
-	 * @access 	protected
-	 * @var    	string
+	 * @since 1.0.0
+	 * @access protected
+	 * @var string
 	 */
 	protected $plugin_name;
 
 	/**
 	 * The version of this plugin.
 	 *
-	 * @since   1.0.0
-	 * @access  protected
-	 * @var     string
+	 * @since 1.0.0
+	 * @access protected
+	 * @var string
 	 */
 	protected $version;
 
 	/**
-	 * The options defining the public-facing fuctionalities.
+	 * Theme support features.
 	 *
-	 * @since  	1.0.0
-	 * @access 	protected
-	 * @var 	object
+	 * @since 1.0.0
+	 * @access protected
+	 * @var array
+	 */
+	protected $support;
+
+	/**
+	 * Options required to define the public-facing fuctionalities.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 * @var object
 	 */
 	protected $options;
 
 	/**
 	 * APIRoutes instance.
 	 *
-	 * @since  	1.0.0
-	 * @access 	protected
-	 * @var 	APIRoutes
+	 * @since 1.0.0
+	 * @access protected
+	 * @var APIRoutes
 	 */
 	private $routes;
 
@@ -71,8 +80,8 @@ final class ViewPublic {
 	 * and run the WordPress Hooks to enqueue styles and JavaScripts
 	 * in the public-facing side, and register custom API Routes
 	 *
-	 * @since  	1.0.0
-	 * @access 	public
+	 * @since 1.0.0
+	 * @access public
 	 *
 	 * @param array $args {
 	 *     An array of common arguments of the plugin.
@@ -95,14 +104,13 @@ final class ViewPublic {
 
 		$this->requires();
 		$this->hooks();
-		$this->setups();
 	}
 
 	/**
 	 * Load the required dependencies.
 	 *
-	 * @since 	1.0.0
-	 * @access 	protected
+	 * @since 1.0.0
+	 * @access protected
 	 */
 	protected function requires() {
 
@@ -114,11 +122,12 @@ final class ViewPublic {
 	/**
 	 * Run Filters and Actions required.
 	 *
-	 * @since 	1.0.0
-	 * @access 	protected
+	 * @since 1.0.0
+	 * @access protected
 	 */
 	protected function hooks() {
 
+		add_action( 'init', array( $this, 'setups' ) );
 		add_action( 'init', array( $this, 'enqueue_styles' ) );
 		add_action( 'init', array( $this, 'enqueue_scripts' ) );
 		add_action( 'init', array( $this, 'register_api_routes' ) );
@@ -130,23 +139,29 @@ final class ViewPublic {
 	 * The setups may involve running some Classes, Functions, and if necessary, WordPress Hooks
 	 * that are required to render the public-facing side.
 	 *
-	 * @since  	1.0.0
-	 * @access 	protected
+	 * @since 1.0.0
+	 * @access protected
 	 */
-	protected function setups() {
+	public function setups() {
 
 		new Buttons( $this->args );
 
 		$this->routes  = new APIRoutes( $this->args, new Metas( $this->args ) );
 
-		$this->options = (object) get_option( "{$this->plugin_opts}_advanced" );
+		$this->support = $this->theme_support( $this->plugin_name );
+
+		$this->options = (object) array(
+			'advanced' => get_option( "{$this->plugin_opts}_advanced" ),
+		);
 	}
 
 	/**
 	 * Register the stylesheets for the public-facing side.
 	 *
-	 * @since 	1.0.0
-	 * @access 	public
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return  mixed
 	 */
 	public function enqueue_styles() {
 
@@ -154,7 +169,7 @@ final class ViewPublic {
 			return;
 		}
 
-		if ( (bool) $this->options->enableStylesheet ) {
+		if ( $this->is_stylesheet() ) {
 			wp_enqueue_style( $this->plugin_name, $this->path_url . 'css/styles.css', array(), $this->version, 'all' );
 		}
 	}
@@ -162,8 +177,8 @@ final class ViewPublic {
 	/**
 	 * Register the JavaScript for the public-facing side of the site.
 	 *
-	 * @since 	1.0.0
-	 * @access 	public
+	 * @since 1.0.0
+	 * @access public
 	 */
 	public function enqueue_scripts() {
 
@@ -177,12 +192,39 @@ final class ViewPublic {
 	/**
 	 * Register custom WP-API Routes
 	 *
-	 * @since 	1.0.0
-	 * @access 	public
+	 * @since 1.0.0
+	 * @access public
 	 */
 	public function register_api_routes() {
 
 		add_filter( 'rest_api_init', array( $this->routes, 'register_routes' ) );
 		add_action( 'wp_enqueue_scripts', array( $this->routes, 'localize_scripts' ) );
+	}
+
+	/**
+	 * Utility function to check if we have to enqueue the stylesheet.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return boolean
+	 */
+	protected function is_stylesheet() {
+
+		$stylesheet = true;
+
+		if ( isset( $this->options->advanced['enableStylesheet'] ) ) {
+			$stylesheet = (bool) $this->options->advanced['enableStylesheet'];
+		}
+
+		if ( isset( $this->support['stylesheet'] ) ) {
+			$stylesheet = (bool) $this->support['stylesheet'];
+		}
+
+		if ( isset( $this->support['attrPrefix'] ) ) {
+			$stylesheet = $this->support['attrPrefix'] === self::get_attr_prefix() ? true : false;
+		}
+
+		return $stylesheet;
 	}
 }
