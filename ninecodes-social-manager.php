76a9e536482c3a1cd335f4a1c3c50771ebd700e8
Thoriq Firdaus <tfirdaus@outlook.com>
2016-11-02 09:58:12 +0700

    Add copyright notice

--- ninecodes-social-manager.php
+++ ninecodes-social-manager.php
@@ -15,6 +15,8 @@
  * Text Domain: ninecodes-social-manager
  * Domain Path: /languages
  *
+ * Copyright (c) 2016 NineCodes (https://ninecodes.com/)
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
