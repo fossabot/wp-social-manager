88041b7e2cc9b489986300dcf2a4a72b85067126
Thoriq Firdaus <tfirdaus@outlook.com>
2016-11-03 11:52:48 +0700

    bump to 1.0.0-beta.6

--- ninecodes-social-manager.php
+++ ninecodes-social-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Social Manager by NineCodes
  * Plugin URI: http://wordpress.org/plugins/ninecodes-social-manager
  * Description: Optimize your website presence in social media.
- * Version: 1.0.0-beta.5
+ * Version: 1.0.0-beta.6
  * Author: Thoriq Firdaus
  * Author URI: https://github.com/tfirdaus
  * License: GPL-2.0+
