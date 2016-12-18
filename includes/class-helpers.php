98b4218b3a3a8cd06f9ac54a61557ea064badd61
Thoriq Firdaus <tfirdaus@outlook.com>
2016-12-18 17:21:09 +0700

    change the icon reference to the inline svg

--- includes/class-helpers.php
+++ includes/class-helpers.php
@@ -43,36 +43,99 @@ class Helpers {
 	final public static function get_social_icons( $name = '' ) {
 
 		$path = plugin_dir_url( dirname( __FILE__ ) );
+		$prefix = esc_attr( self::get_attr_prefix() );
 
-		$facebook = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#facebook /></svg>';
-		$twitter = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#twitter /></svg>';
-		$instagram = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#instagram /></svg>';
-		$pinterest = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#pinterest /></svg>';
-		$linkedin = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#linkedin /></svg>';
-		$googleplus = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#googleplus /></svg>';
-		$youtube = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#youtube /></svg>';
-		$reddit = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#reddit /></svg>';
-		$dribbble = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#dribbble /></svg>';
-		$behance = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#behance /></svg>';
-		$github = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#github /></svg>';
-		$codepen = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#codepen /></svg>';
-		$email = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><use xlink:href=' . $path . 'public/img/social-sites-icon.svg#email /></svg>';
+		$facebook = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-facebook'/></svg>";
+		$twitter = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-twitter'/></svg>";
+		$instagram = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-instagram'/></svg>";
+		$pinterest = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-pinterest'/></svg>";
+		$linkedin = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-linkedin'/></svg>";
+		$googleplus = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-googleplus'/></svg>";
+		$youtube = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-youtube'/></svg>";
+		$reddit = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-reddit'/></svg>";
+		$dribbble = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-dribbble'/></svg>";
+		$behance = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-behance'/></svg>";
+		$github = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-github'/></svg>";
+		$codepen = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-codepen'/></svg>";
+		$email = "<svg aria-hidden='true'><use xlink:href='#{$prefix}-icon-email'/></svg>";
 
 		$icons = array(
-			'facebook' => apply_filters( 'ninecodes_social_manager_icon', $facebook, 'facebook', false ),
-			'twitter' => apply_filters( 'ninecodes_social_manager_icon', $twitter, 'twitter', false ),
-			'instagram' => apply_filters( 'ninecodes_social_manager_icon', $instagram, 'instagram', false ),
-			'pinterest' => apply_filters( 'ninecodes_social_manager_icon', $pinterest, 'pinterest', false ),
-			'linkedin' => apply_filters( 'ninecodes_social_manager_icon', $linkedin, 'linkedin', false ),
-			'googleplus' => apply_filters( 'ninecodes_social_manager_icon', $googleplus, 'googleplus', false ),
-			'youtube' => apply_filters( 'ninecodes_social_manager_icon', $youtube, 'youtube', false ),
-			'reddit' => apply_filters( 'ninecodes_social_manager_icon', $reddit, 'reddit', false ),
-			'dribbble' => apply_filters( 'ninecodes_social_manager_icon', $dribbble, 'dribbble', false ),
-			'behance' => apply_filters( 'ninecodes_social_manager_icon', $behance, 'behance', false ),
-			'github' => apply_filters( 'ninecodes_social_manager_icon', $github, 'github', false ),
-			'codepen' => apply_filters( 'ninecodes_social_manager_icon', $codepen, 'codepen', false ),
-			'email' => apply_filters( 'ninecodes_social_manager_icon', $email, 'email', false ),
+			'facebook' => apply_filters( 'ninecodes_social_manager_icon', $facebook, array(
+				'site' => 'facebook',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'twitter' => apply_filters( 'ninecodes_social_manager_icon', $twitter, array(
+				'site' => 'twitter',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'instagram' => apply_filters( 'ninecodes_social_manager_icon', $instagram, array(
+				'site' => 'instagram',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'pinterest' => apply_filters( 'ninecodes_social_manager_icon', $pinterest, array(
+				'site' => 'pinterest',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'linkedin' => apply_filters( 'ninecodes_social_manager_icon', $linkedin, array(
+				'site' => 'linkedin',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'googleplus' => apply_filters( 'ninecodes_social_manager_icon', $googleplus, array(
+				'site' => 'googleplus',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'youtube' => apply_filters( 'ninecodes_social_manager_icon', $youtube, array(
+				'site' => 'youtube',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'reddit' => apply_filters( 'ninecodes_social_manager_icon', $reddit, array(
+				'site' => 'reddit',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'dribbble' => apply_filters( 'ninecodes_social_manager_icon', $dribbble, array(
+				'site' => 'dribbble',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'behance' => apply_filters( 'ninecodes_social_manager_icon', $behance, array(
+				'site' => 'behance',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'github' => apply_filters( 'ninecodes_social_manager_icon', $github, array(
+				'site' => 'github',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'codepen' => apply_filters( 'ninecodes_social_manager_icon', $codepen, array(
+				'site' => 'codepen',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
+			'email' => apply_filters( 'ninecodes_social_manager_icon', $email, array(
+				'site' => 'email',
+				'prefix' => $prefix,
+				'context' => false,
+			) ),
 		);
+
+		/**
+		 * Hooks to filter all the icons at once.
+		 *
+		 * @var array
+		 */
+		$icons = apply_filters( 'ninecodes_social_manager_icons', $icons, array(
+			'prefix' => 'prefix',
+			'context' => false,
+		) );
 
 		$output = isset( $icons[ $name ] ) ? $icons[ $name ] : $icons;
 
