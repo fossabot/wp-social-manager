29e9e840c2a5b05c13d944760cf42162fb53a715
Thoriq Firdaus <tfirdaus@outlook.com>
2017-02-11 18:54:16 +0700

    Refine scripts naming for custom fields

--- admin/js/field-checkbox-toggle.js
+++ admin/js/field-checkbox-toggle.js
@@ -12,7 +12,7 @@ jQuery(function($) {
 		toggleControls;
 
 	Toggle.View = Backbone.View.extend({
-		el: $('#ninecodes-social-manager-wrap').find('.toggle-control')
+		el: $('#ninecodes-social-manager-wrap').find('.field-checkbox-toggle')
 	});
 
 	ToggleControls = Toggle.View.extend({
