d822c985e2e665eeede8e2598b1e023c3bd4f17d
Thoriq Firdaus <tfirdaus@outlook.com>
2017-03-13 14:31:07 +0700

    Add array_insert function
    Insert an array into another array before/after a certain key.

--- includes/function-utilities.php
+++ includes/function-utilities.php
@@ -120,3 +120,33 @@ function wp_parse_args_recursive( &$a, $b ) {
 
 	return $result;
 }
+
+/**
+ * Insert an array into another array before/after a certain key
+ *
+ * @since 1.2.0
+ *
+ * @param array  $array 	The initial array.
+ * @param array  $pairs 	The array to insert.
+ * @param string $key 		The certain key.
+ * @param string $position 	Wether to insert the array before or after the key.
+ * @return array
+ */
+function array_insert( $array, $pairs, $key, $position = 'after' ) {
+
+	$key_pos = array_search( $key, array_keys( $array ), true );
+
+	if ( 'after' === $position ) {
+		$key_pos++;
+	}
+
+	if ( false !== $key_pos ) {
+		$result = array_slice( $array, 0, $key_pos );
+		$result = array_merge( $result, $pairs );
+		$result = array_merge( $result, array_slice( $array, $key_pos ) );
+	} else {
+		$result = array_merge( $array, $pairs );
+	}
+
+	return $result;
+}
