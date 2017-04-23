2a78d65c8a98d15a3fab33820a1d24f8104ef2af
Thoriq Firdaus <tfirdaus@outlook.com>
2017-04-24 05:00:24 +0700

    build(vvv): update project path in local vvv

--- gruntfile.js
+++ gruntfile.js
@@ -116,7 +116,7 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		config: {
-			plugin_path: '/srv/www/wordpress-default/public_html/wp-content/plugins/<%= pkg.name %>' // Plugins path in VVV.
+			plugin_path: '/srv/www/wordpress-develop/public_html/src/wp-content/plugins/<%= pkg.name %>' // Plugins path in VVV.
 		},
 
 		// Shell actions.
